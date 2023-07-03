c885976a222145748f3a2c2e53de03e039524903
Iruko <irukoa@live.com>
2023-07-03 17:51:28 +0200

    bug: toy model corrected.

--- src/main.F90
+++ src/main.F90
@@ -20,15 +20,23 @@ program floquet_tight_binding
 
   type(local_k_data), allocatable :: H(:)!TEST
 
+  integer :: i_mem
+  integer, allocatable :: i_arr(:)
+
   open(unit=112, action="write", file="exec.out")
 
   call OMP_SET_MAX_ACTIVE_LEVELS(2)
 
-  !a = sys_constructor("GaAs", "./")
   a = sys_constructor("HM", "./")
 
-  call get_hamiltonian(system = a, k = (/0.0_dp, 0.0_dp, 0.0_dp/), H = H, Nder_i = 0, only_i = .false.)!TEST
-  print*, H(1)%k_data
+  call get_hamiltonian(system = a, k = (/0.0_dp, 0.0_dp, 0.0_dp/), H = H, Nder_i = 1, only_i = .false.)!TEST
+  print*, H(1)%k_data!Hamiltonian 0th derivative.
+  print*, ""
+  allocate(i_arr(product(H(2)%integer_indices)))
+  do i_mem = 1, product(H(2)%integer_indices)
+    write(*, fmt="(3I2, 2F8.5)"), integer_memory_element_to_array_element(H(2), i_mem), &
+    real(H(2)%k_data(i_mem), dp), aimag(H(2)%k_data(i_mem))!Hamiltonian 1st Derivative.
+  enddo
 
   !EXAMPLE OF USAGE.
   test = task_constructor(name           = "ext_ben", &
