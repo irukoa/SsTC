21052b0cc3ebf250aabf100ace3d89774f12316f
Irukoa <irukoa@live.com>
2023-09-23 15:41:33 +0200

    feat: integrator feats target offload. Better error coord.

--- src/cond_comp.h
+++ src/cond_comp.h
@@ -1,7 +1,11 @@
 #ifdef _OPENMP
-#define using_OMP  .true.
 #define _OMPTGT_(x) $OMP x
+#ifdef _OMPOFFLOADTGT
+#define _OMPOFFLOADTGT_(x) $OMP x
 #else
-#define using_OMP  .false.
+#define _OMPOFFLOADTGT_(x) disabled
+#endif
+#else
 #define _OMPTGT_(x) disabled
+#define _OMPOFFLOADTGT_(x) disabled
 #endif
