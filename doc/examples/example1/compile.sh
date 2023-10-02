a537e6fb794fd7d92e10b4348febff5f773f2dd7
Irukoa <irukoa@live.com>
2023-10-02 13:25:35 +0200

    fix: -Ofast in examples.

--- doc/examples/example1/compile.sh
+++ doc/examples/example1/compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mpiifort -qopenmp -lmkl_intel_lp64 -lmkl_core -lmkl_gnu_thread -lpthread -O2 example1.F90 -I../../../bin/ ../../../bin/libSsTC.a -o "test.x"
+mpiifort -qopenmp -lmkl_intel_lp64 -lmkl_core -lmkl_gnu_thread -lpthread -Ofast example1.F90 -I../../../bin/ ../../../bin/libSsTC.a -o "test.x"
